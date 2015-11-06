65490993f27adbb41623c1a89a7dc4eca7cf5fcb
Anirvan Chakraborty <anirvanc@cakesolutions.net>
2015-11-06 20:12:18 +0000

    Updated the python env setup with Spark

--- analytics/init-env.sh
+++ analytics/init-env.sh
@@ -10,7 +10,7 @@ fi
 # Create virtual env
 VENV=venv
 sudo pip2.7 install virtualenv
-rm -rf $VENV
+sudo rm -rf $VENV
 virtualenv $VENV -p /usr/bin/python2.7
 source $VENV/bin/activate
 
@@ -30,17 +30,19 @@ cd -
 
 # Install Spark
 cd $VENV
-#curl http://www.eu.apache.org/dist/spark/spark-1.5.1/spark-1.5.1-bin-hadoop2.6.tgz -o spark-1.5.1-bin-hadoop2.6.tgz
-#if ! [ `command -v pv` ]
-#then
-#  brew install pv
-#fi
-#[ $(pv spark-1.5.1-bin-hadoop2.6.tgz | tar xzf -) ] || tar xzf spark-1.5.1-bin-hadoop2.6.tgz
+
+if ! [ `command -v pv` ]
+then
+  brew install pv
+fi
+curl http://mirror.catn.com/pub/apache/spark/spark-1.5.1/spark-1.5.1-bin-hadoop2.6.tgz -o spark-1.5.1-bin-hadoop2.6.tgz
+[ $(pv spark-1.5.1-bin-hadoop2.6.tgz | tar xzf -) ] || tar xzf spark-1.5.1-bin-hadoop2.6.tgz
+
 cd -
+SPARK_HOME=`pwd`/$VENV/spark-1.5.1-bin-hadoop2.6
 cat >> $VENV/bin/activate << EOF
-export SPARK_HOME=`pwd`/$VENV/spark-1.5.1-bin-hadoop2.6
+export SPARK_HOME=$SPARK_HOME
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/build:$PYTHONPATH
-PATH=$SPARK_HOME/bin:$PATH
+export PATH=$SPARK_HOME/bin:$PATH
 EOF
 source $VENV/bin/activate
-
